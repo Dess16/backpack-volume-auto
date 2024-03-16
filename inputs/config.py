c05817187002ac64f6b3d18d6d79d618d5d2a4d4
Dess16 <93867858+Dess16@users.noreply.github.com>
2024-03-16 12:02:11 +0300

    Update config.py

--- inputs/config.py
+++ inputs/config.py
@@ -1,6 +1,6 @@
 THREADS = 1  # Enter amount of threads
-DELAY_BETWEEN_TRADE = (1, 2)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
-DELAY_BETWEEN_DEAL = (0, 0)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
+DELAY_BETWEEN_TRADE = (30, 100)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
+DELAY_BETWEEN_DEAL = (0, 60)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
 
 NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0  # min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
@@ -10,7 +10,7 @@ ALLOWED_ASSETS = ["SOL_USDC", "PYTH_USDC", "JTO_USDC", "HNT_USDC", "MOBILE_USDC"
 
 # Volatility moment
 # DEPTH of limit order to trade as market order
-DEPTH = 3  # 1-20, optimal - 3-5, recommend up to 10, bigger depth = better market order but more slippage
+DEPTH = 5  # 1-20, optimal - 3-5, recommend up to 10, bigger depth = better market order but more slippage
 
 
 
